def5b8492172d76d164bbc990d054d7eb8ebee4b
aellis14 <adriana.ellis@patelhs.org>
2020-11-14 01:10:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+class Main {
+  public static void main(String[] args) {
+    String[] names = {"Sarah","Nick","Jacob","Jenny","Luke","Alexa","lil kid","John","Timmy"};
+
+  
+    names [6] = "big kid";
+
+    System.out.println("\n Here is our updated array: ");
+
+    for (int i=8; i>0; i--)
+    {
+  
+    int[ ] numbers = {-20, -5, 11, 4, 17, 43, 59, 62, 71, 86};
+
+   
+      //only print the numberif it is even
+     if(numbers[i] % 2 == 0){
+      System.out.println("This number is even: " + numbers[i]);
+     }else{
+      //anything that enters the else is an odd number
+      numbers[i] = 0;
+    }
+  }
+  for(int i = 9;i >= 0;i--){
+  }
+
+  }
+}
